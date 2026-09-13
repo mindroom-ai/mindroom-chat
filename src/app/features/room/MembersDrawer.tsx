5894a5199d610a71e9ed19b7d82fb3c9510731bb
Bas Nijholt <bas@nijho.lt>
2026-09-13 07:57:46 -0700

    feat: add members drawer invite button

--- src/app/features/room/MembersDrawer.tsx
+++ src/app/features/room/MembersDrawer.tsx
@@ -59,22 +59,50 @@ import { useSpaceOptionally } from '../../hooks/useSpace';
 import { ContainerColor } from '../../styles/ContainerColor.css';
 import { useFlattenPowerTagMembers, useGetMemberPowerTag } from '../../hooks/useMemberPowerTag';
 import { useRoomCreators } from '../../hooks/useRoomCreators';
+import { useRoomPermissions } from '../../hooks/useRoomPermissions';
+import { InviteUserPrompt } from '../../components/invite-user-prompt';
 
 type MemberDrawerHeaderProps = {
   room: Room;
+  canInvite: boolean;
 };
-function MemberDrawerHeader({ room }: MemberDrawerHeaderProps) {
+function MemberDrawerHeader({ room, canInvite }: MemberDrawerHeaderProps) {
   const setPeopleDrawer = useSetSetting(settingsAtom, 'isPeopleDrawer');
+  const [invitePrompt, setInvitePrompt] = useState(false);
 
   return (
     <Header className={css.MembersDrawerHeader} variant="Background" size="600">
+      {invitePrompt && <InviteUserPrompt room={room} requestClose={() => setInvitePrompt(false)} />}
       <Box grow="Yes" alignItems="Center" gap="200">
         <Box grow="Yes" alignItems="Center" gap="200">
           <Text title={`${room.getJoinedMemberCount()} Members`} size="H5" truncate>
             {`${millify(room.getJoinedMemberCount())} Members`}
           </Text>
         </Box>
-        <Box shrink="No" alignItems="Center">
+        <Box shrink="No" alignItems="Center" gap="100">
+          <TooltipProvider
+            position="Bottom"
+            align="End"
+            offset={4}
+            tooltip={
+              <Tooltip>
+                <Text>Invite</Text>
+              </Tooltip>
+            }
+          >
+            {(triggerRef) => (
+              <IconButton
+                ref={triggerRef}
+                aria-label="Invite people"
+                aria-pressed={invitePrompt}
+                disabled={!canInvite}
+                variant="Background"
+                onClick={() => setInvitePrompt(true)}
+              >
+                <Icon src={Icons.UserPlus} />
+              </IconButton>
+            )}
+          </TooltipProvider>
           <TooltipProvider
             position="Bottom"
             align="End"
@@ -186,6 +214,8 @@ export function MembersDrawer({ room, members }: MembersDrawerProps) {
   const creators = useRoomCreators(room);
   const getPowerTag = useGetMemberPowerTag(room, creators, powerLevels);
   const getPowerLevel = useGetMemberPowerLevel(powerLevels);
+  const permissions = useRoomPermissions(creators, powerLevels);
+  const canInvite = permissions.action('invite', mx.getSafeUserId());
 
   const fetchingMembers = members.length < room.getJoinedMemberCount();
   const openUserRoomProfile = useOpenUserRoomProfile();
@@ -250,7 +280,7 @@ export function MembersDrawer({ room, members }: MembersDrawerProps) {
       shrink="No"
       direction="Column"
     >
-      <MemberDrawerHeader room={room} />
+      <MemberDrawerHeader room={room} canInvite={canInvite} />
       <Box className={css.MemberDrawerContentBase} grow="Yes">
         <Scroll ref={scrollRef} variant="Background" size="300" visibility="Hover" hideTrack>
           <Box className={css.MemberDrawerContent} direction="Column" gap="200">
